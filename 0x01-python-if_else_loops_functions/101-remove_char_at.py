284b193b143a11b358ff214b105d1e8126898c23
fourbet <ofourbet@yahoo.fr>
2020-12-08 20:11:58 +0000

    add remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,6 +3,6 @@ def remove_char_at(str, n):
     count = 0
     for c in str:
         if count == n:
-            str = str[:n] + str[n-1:]
+            str = str[:n] + str[(n+1):]
         count += 1
     return (str)
