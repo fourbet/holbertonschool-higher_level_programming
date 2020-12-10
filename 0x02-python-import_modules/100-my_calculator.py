ed98fc4f6341c4b81a145937d42cd9aa7a55d88c
fourbet <ofourbet@yahoo.fr>
2020-12-10 13:29:01 +0000

    build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    nbr = len(sys.argv) - 1
+    if nbr != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    if op == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == "_":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
