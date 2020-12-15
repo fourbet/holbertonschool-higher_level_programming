5c9a6b1734eff0154e9b401aae403d3b6305349e
fourbet <ofourbet@yahoo.fr>
2020-12-15 14:46:22 +0000

    find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = 0
-    if not my_list:
+    if my_list:
+        for i in range(len(my_list)):
+            if max < my_list[i]:
+                max = my_list[i]
+        return max
+    else:
         return None
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
