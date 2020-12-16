9bb29f54ae170a6e386aa8ef530af9263d305f42
fourbet <ofourbet@yahoo.fr>
2020-12-16 13:11:50 +0000

    correct tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c=()
     if len(tuple_a) == 1:
-        tuple_a = tuple_a[0] + (0)
+        la = list(tuple_a)
+        la.append(0)
+        tuple_a = tuple(la)
     if len(tuple_a) == 0:
         tuple_a = 0, 0
     if len(tuple_b) == 1:
-        tuple_b = tuple_b[0] + (0)
+        lb = list(tuple_b)
+        lb.append(0)
+        tuple_b = tuple(lb)
     if tuple_b == ():
         tuple_b = 0, 0
     tuple_c = tuple_a[0] + tuple_b[0] , tuple_a[1] + tuple_b[1]
