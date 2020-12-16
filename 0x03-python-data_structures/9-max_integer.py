16c582a178c8250cd097b1a5d9f0af666fe1f6a0
fourbet <ofourbet@yahoo.fr>
2020-12-16 12:32:09 +0000

    add case number neg

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,7 @@ def max_integer(my_list=[]):
     max = 0
     if my_list:
         for i in range(len(my_list)):
+            max = my_list[0]
             if max < my_list[i]:
                 max = my_list[i]
         return max
