8fa5a21c397eca78edc4cf664f5eb0b7bd57cbba
fourbet <ofourbet@yahoo.fr>
2020-12-16 13:13:52 +0000

    correct pep8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c=()
+    tuple_c = ()
     if len(tuple_a) == 1:
         la = list(tuple_a)
         la.append(0)
@@ -13,5 +13,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple(lb)
     if tuple_b == ():
         tuple_b = 0, 0
-    tuple_c = tuple_a[0] + tuple_b[0] , tuple_a[1] + tuple_b[1]
+    tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return(tuple_c)
