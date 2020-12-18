e22994517532b1f755183c8b944a618d718826f1
fourbet <ofourbet@yahoo.fr>
2020-12-18 14:27:49 +0000

    add case I before V

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,12 @@
 def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
+    count = 0
     for nbr in roman_string:
         for k, v in dic.items():
+            if nbr == 'I' and count < len(roman_string) :
+                sum -= 1
             if nbr == k:
                 sum += v
+            count+= 1
     return sum
