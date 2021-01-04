5ebe879a78332d0f73e5c128dac7f210a471c45b
fourbet <ofourbet@yahoo.fr>
2021-01-04 16:35:58 +0000

    add safe print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as e:
+        sys.stderr.write("Exception: {}".format(e))
+        return False
+    else:
+        return True
