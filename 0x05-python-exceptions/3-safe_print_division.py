fd06a5397d272bb3e7fb50af2d0e51c7506b8e64
fourbet <ofourbet@yahoo.fr>
2021-01-04 16:01:10 +0000

    add safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except:
         pass
     finally:
-        print("Inside result : {}".format(res))
+        print("Inside result: {}".format(res))
     return res
