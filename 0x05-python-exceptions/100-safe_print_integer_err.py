50ce5910edf0c92f8e4c8a489e25c331b51de879
fourbet <ofourbet@yahoo.fr>
2021-01-04 16:40:16 +0000

    change for sys.exc_info

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as e:
-        sys.stderr.write("Exception: {}".format(e))
+    except:
+        sys.stderr.write("Exception: {}".format(sys.exc_info()[0]))
         return False
     else:
         return True
