5cb3a35dff4ff529e268e308fbd7ee92db98f15f
fourbet <ofourbet@yahoo.fr>
2021-01-05 19:24:09 +0000

    add radius in init instanciation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import dis
 """ Module doc """
 
 
 class MagicClass:
     """ Magic calculation """
-    def __init__(self):
+    def __init__(self, radius):
         """ initialization """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not (float):
@@ -19,3 +20,4 @@ class MagicClass:
     def circumference(self):
         """ circumference """
         return 2 * math.pi * self.__radius
+dis.dis(MagicClass)
