f67bbd31682df2d4344b9dc9032cceabaf80bbfb
fourbet <ofourbet@yahoo.fr>
2021-01-05 17:09:38 +0000

    add condition tuples positives

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,7 +15,7 @@ class Square:
               not isinstance(position[1], int)):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif position[0] < 0 or position[1] < 0:
-             raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
 
