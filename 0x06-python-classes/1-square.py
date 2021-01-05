2fcda22d2d874d5535cd2fc53349a43267810ac2
fourbet <ofourbet@yahoo.fr>
2021-01-05 14:51:13 +0000

    add module doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-""" Class square defines a square """
+    """ Class square defines a square """
     def __init__(self, size=0):
-    """ initialization of an instance Square """
+        """ initialization of an instance Square """
         self.__size = size
