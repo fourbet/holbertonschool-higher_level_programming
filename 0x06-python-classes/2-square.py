dc41f769f8d8d078a7fb922729ee6e499f58d27b
fourbet <ofourbet@yahoo.fr>
2021-01-05 13:04:58 +0000

    add size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    "Class square that defines a square"
+    def __init__(self, size=0):
+        """ initialization of the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
