ba22dbcc504c59c08ead2a091fac9d0009776636
fourbet <ofourbet@yahoo.fr>
2021-01-05 19:18:17 +0000

    add magic calculation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,7 +3,9 @@
 
 
 class MagicClass:
+    """ Magic calculation """
     def __init__(self):
+        """ initialization """
         self.__radius = 0
         if type(radius) is not (int) or (type(radius) is not (float)):
             raise TypeError("radius must be a number")
@@ -11,7 +13,9 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """ area """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """ circumference """
         return 2 * math.pi * self.__radius
