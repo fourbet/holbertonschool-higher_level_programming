03f3f892464ea06058d020c5395557e8cc29a871
fourbet <ofourbet@yahoo.fr>
2021-01-05 13:51:55 +0000

    add coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    "Class square that defines a square"
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ initialization of the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif (not isinstance(position[0], int)and
+              not isinstance(position[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints in stdout the square with the character # """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def size(self):
+        """ retieve size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
