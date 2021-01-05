aee05d2b6d0d0031f0ac843f39f0e3ee739f2aa7
fourbet <ofourbet@yahoo.fr>
2021-01-05 19:21:56 +0000

    modify or into and

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
     def __init__(self):
         """ initialization """
         self.__radius = 0
-        if type(radius) is not (int) or (type(radius) is not (float)):
+        if type(radius) is not int and type(radius) is not (float):
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
