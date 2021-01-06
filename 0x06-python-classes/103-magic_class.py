55f4b377ae5dfdd793b99d7cf8af21292177370d
fourbet <ofourbet@yahoo.fr>
2021-01-06 08:17:32 +0000

    add init radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """ Magic calculation """
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """ initialization """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not (float):
