1bd92cfe5fbf0c3128be7e64330df8dbfba181bb
fourbet <ofourbet@yahoo.fr>
2021-01-06 13:24:14 +0000

    add __str__ method

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" Module doc """
+
+
+class Square:
+    """ Class square that defines a square """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ initialization of the class square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif (len(position) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not isinstance(position[0], int) or
+              not isinstance(position[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ prints in stdout the square with the character # """
+        if self.__size == 0:
+            print()
+            return
+        res = ""
+        for l in range(self.__position[1]):
+            res += "\n"
+        for i in range(self.__size):
+            for k in range(self.__position[0]):
+                res += " "
+            for j in range(self.__size):
+                res += "#"
+            res += "\n"
+        res = res[:-1]
+        return res
+
+    @property
+    def size(self):
+        """ retieve size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ retrieve position """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ set position """
+        if (not isinstance(position[0], int) or
+                not isinstance(position[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
