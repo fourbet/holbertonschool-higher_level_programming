31d96c4dadee8a7b9ed54c0e6daf19ad5780af6f
fourbet <ofourbet@yahoo.fr>
2021-01-07 15:38:18 +0000

    correct pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,9 +9,9 @@ def matrix_divided(matrix, div):
         size = len(matrix[0])
         if size != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (float,int)):
+    if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    mat = [[round(x/div, 2) for x in mat[i]] for i in range(len(mat))] 
+    mat = [[round(x/div, 2) for x in mat[i]] for i in range(len(mat))]
     return mat
