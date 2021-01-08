814ba8fc18931be641e8249bfc76eb85aacaa4a4
fourbet <ofourbet@yahoo.fr>
2021-01-08 20:52:44 +0000

    add case not a list of list of int or float

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,6 +6,10 @@ def matrix_divided(matrix, div):
     """ divides all elments of a matrix """
     mat = matrix[:]
     for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in range(len(matrix)):
         size = len(matrix[0])
         if size != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
