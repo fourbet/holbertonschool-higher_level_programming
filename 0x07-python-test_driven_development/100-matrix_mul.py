123fbce6a0bd8f4879cea158937f8d4c67ee0001
fourbet <ofourbet@yahoo.fr>
2021-01-08 14:28:31 +0000

    add mul matrix

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Module doc """
+
+
+def matrix_mul(m_a, m_b):
+    """ multiplies  matrices without using any modules """
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    for i in range(len(m_a)):
+        if type((m_a[i])) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for i in range(len(m_b)):
+        if type((m_b[i])) is not list:
+            raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    for i in range(len(m_a)):
+        for j in range(len(m_a[i])):
+            if not isinstance(m_a[i][j], (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for i in range(len(m_b)):
+        for j in range(len(m_b[i])):
+            if not isinstance(m_b[i][j], (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+    for i in range(len(m_a)):
+        size_a = len(m_a[0])
+        if len(m_a[i]) != size_a:
+            raise TypeError("each row of m_a must be of the same size")
+    for i in range(len(m_b)):
+        size_b = len(m_b[0])
+        if len(m_b[i]) != size_b:
+            raise TypeError("each row of m_b must be of the same size")
+    row_b = len(m_b)
+    column_a = len(m_a[0])
+    if row_b != column_a:
+        raise ValueError("m_a and m_b can't be multiplied")
+    row_res = len(m_a)
+    column_res = len(m_b[0])
+    res = [[0 for x in range(column_res)] for y in range(row_res)]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                res[i][j] += m_a[i][k] * m_b[k][j]
+    return res
