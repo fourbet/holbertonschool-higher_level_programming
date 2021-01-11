b290118456f7dc43cbe3f6078b40ff537f2d3372
fourbet <ofourbet@yahoo.fr>
2021-01-11 12:37:06 +0000

    add doc str

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,6 +50,7 @@ class Rectangle:
             return (self.__width * 2 + self.__height * 2)
 
     def __str__(self):
+        """ informal string representation """
         rec = ""
         if self.__width == 0 or self.__height == 0:
             return ""
