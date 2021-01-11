c2341bca9f3b805c12d27c194105071b278d89e7
fourbet <ofourbet@yahoo.fr>
2021-01-11 11:33:00 +0000

    add simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Simple rectangle """
+
+
+class Rectangle:
+    """ class Rectangle defines a rectangle """
+
+    def __init__(self):
+        """ initialization """
+        pass
