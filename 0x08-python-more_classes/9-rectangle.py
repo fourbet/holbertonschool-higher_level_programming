f2c7727d943e4977a027a29ce22de38303c8e925
fourbet <ofourbet@yahoo.fr>
2021-01-11 17:34:33 +0000

    correct setter methods

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ initialization """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         type(self).number_of_instances += 1
 
     @property
@@ -22,9 +22,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ width setter """
-        if type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -36,9 +36,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ height setter """
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
