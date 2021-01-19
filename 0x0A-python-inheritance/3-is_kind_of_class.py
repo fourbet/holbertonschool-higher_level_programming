cb5f43c533e6c1e2f19dd24e3f7e053908f36952
fourbet <ofourbet@yahoo.fr>
2021-01-19 12:32:24 +0000

    add same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Project: Python - Inheritance """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks if obj is an instance of or an instance of the class
+    inherited from, a_class """
+    return isinstance(obj, a_class)
