75b9a88bc00a61665afbbe75a9d7b20b1139f7d2
fourbet <ofourbet@yahoo.fr>
2021-01-19 12:52:56 +0000

    add improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Project: Python - Inheritance """
+
+
+class BaseGeometry:
+    """ Class BaseGeometry """
+
+    def area(self):
+        """ Raises Error """
+        raise Exception("area() is not implemented")
