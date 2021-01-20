79a07814400da0481190c8e9d3a3f2a1803676d4
fourbet <ofourbet@yahoo.fr>
2021-01-20 18:03:29 +0000

    add student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Project Python - Input Output """
+
+
+class Student:
+    """ class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initialization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student obj"""
+        if attrs != None:
+            if all(isinstance(att, str) for att in attrs):
+                return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
