b4de9f7ff1b300bec4c5ae11e575cb1345398034
fourbet <ofourbet@yahoo.fr>
2021-01-20 14:05:12 +0000

    add save object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+""" Project: Python Input-Output """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using a JSON representation"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
