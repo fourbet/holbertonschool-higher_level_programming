1cfb0e02a2d43594939b3a1ddcd1f9a553f8233e
fourbet <ofourbet@yahoo.fr>
2021-01-21 08:49:54 +0000

    add import line

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Project: Python - Inheritance """
 
-Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Class BaseGeometry """
