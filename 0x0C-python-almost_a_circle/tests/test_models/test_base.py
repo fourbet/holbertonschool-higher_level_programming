28221bceaf7732faf2b4a3c9a481714d6dd9b50d
fourbet <ofourbet@yahoo.fr>
2021-01-26 11:10:29 +0000

    add test save_to_file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -21,3 +21,12 @@ class TestBase(unittest.TestCase):
 
     def test_nb_objects(self):
         self.assertEqual(2, Base._Base__nb_objects)
+
+    def test_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(dictionary, {'x': 2, 'width': 10, 'id': 1,
+                                      'height': 7, 'y': 8})
+        self.assertIsInstance(dictionary, dict)
+        self.assertIsInstance(json_dictionary, str)
