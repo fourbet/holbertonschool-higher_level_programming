4fb481fa0e63ac4f9449f9bc3a2c766a842a16e7
fourbet <ofourbet@yahoo.fr>
2021-01-26 09:34:09 +0000

    (to_json_dictionaries) add case dict empty

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of list_dictionaries """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == {}:
             return []
         else:
             return json.dumps(list_dictionaries)
