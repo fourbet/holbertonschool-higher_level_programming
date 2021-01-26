060e00e77a4b4ead96ebe5ac404daf99899d63e5
fourbet <ofourbet@yahoo.fr>
2021-01-26 09:38:32 +0000

    (to_json_dictionaries) return string []

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None or list_dictionaries == {}:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
