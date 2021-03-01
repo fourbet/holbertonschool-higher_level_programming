b0e35e4e07651e9b20066da49b9e2358223043b9
fourbet <ofourbet@yahoo.fr>
2021-03-01 13:44:20 +0000

    full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
 -- prints description
-SHOW COLUMNS FROM first_table;
+SHOW FULL COLUMNS FROM first_table;
