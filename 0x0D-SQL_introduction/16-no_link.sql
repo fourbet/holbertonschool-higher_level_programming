96d5aa79e8b96ad5f8dcfae977795050ec2ddcde
fourbet <ofourbet@yahoo.fr>
2021-03-01 18:54:32 +0000

    say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT score, name IF EXISTS FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
