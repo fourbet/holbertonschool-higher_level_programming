9d48c927e56bef5f9aa5887dc01003208f560c94
fourbet <ofourbet@yahoo.fr>
2021-03-02 16:28:46 +0000

    genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+--  lists all shows contained in the database hbtn_0d_tvshows
+SELECT DISTINCT s.title, g.genre_id 
+FROM tv_shows s 
+LEFT JOIN tv_show_genres g 
+ON g.show_id = s.id 
+ORDER BY s.title ,g.genre_id;
