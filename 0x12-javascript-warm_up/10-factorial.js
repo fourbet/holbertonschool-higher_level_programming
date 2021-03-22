e92b01d570682fde8689efb5962251c47be3d28d
fourbet <ofourbet@yahoo.fr>
2021-03-22 14:13:45 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 function fact (nbr) {
-  nbrInt = parseInt(nbr);
-  if (nbrInt === 0 || nbrInt === 1) {
+  if (nbr === 0 || nbr === 1) {
     return 1;
-  }
-  else {
-  return (nbrInt * fact (nbrInt - 1));
+  } else {
+    return (nbr * fact(nbr - 1));
   }
 }
-let res = fact (process.argv[2]);
+const res = fact(parseInt(process.argv[2]));
 console.log(res);
