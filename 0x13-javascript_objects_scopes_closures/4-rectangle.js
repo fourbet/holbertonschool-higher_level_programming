98a758d0686eea5a8a77fa7a58426f43cc82c433
fourbet <ofourbet@yahoo.fr>
2021-03-23 15:38:54 +0000

    feat()

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.width; i++) {
+      rect += 'X';
+    } for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
