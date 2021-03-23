37ba66584f3765dc94201cdf32e4bed14eccf50b
fourbet <ofourbet@yahoo.fr>
2021-03-23 16:19:18 +0000

    feat()

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/bin/usr/node
+exports.nbOccurences = function (list, searchElement) {
+  let nbr = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      nbr += 1;
+    }
+  }
+  return nbr;
+};
