25ee6dcb3a57ecea9c9ca65067a0f9889537e8bd
fourbet <ofourbet@yahoo.fr>
2021-03-28 12:37:03 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,4 +12,3 @@ class City(Base):
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    
