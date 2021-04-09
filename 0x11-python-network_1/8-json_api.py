afced29209c24ddd34fb7865bb6a117bdc4a9936
fourbet <ofourbet@yahoo.fr>
2021-04-09 20:55:48 +0000

    feat(): search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python Network #1 Project """
+import requests as r
+import sys
+
+if __name__ == "__main__":
+    values = {'q': sys.argv[1] if len(argv) > 1 else ""}
+    req = r.post("http://0.0.0.0:5000/search_user", data=values)
+    try:
+        json = req.json()
+        if json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
