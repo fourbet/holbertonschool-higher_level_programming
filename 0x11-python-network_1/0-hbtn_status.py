ec91094415b9b6fd04e58eb5351b5215ec7b42b0
fourbet <ofourbet@yahoo.fr>
2021-04-09 10:29:12 +0000

    feat(): status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python Network #1 Project """
+import urllib.request as ur
+
+if __name__ == "__main__": 
+    req = ur.Request('https://intranet.hbtn.io/status')
+    with ur.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode()))
