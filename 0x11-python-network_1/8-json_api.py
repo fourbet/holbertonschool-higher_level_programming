00f90c59f3cbfd2a0f0297dec43b9167d99bb929
fourbet <ofourbet@yahoo.fr>
2021-04-09 20:59:56 +0000

    feat(): search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,7 @@
 import requests as r
 import sys
 
+
 if __name__ == "__main__":
     values = {'q': sys.argv[1] if len(argv) > 1 else ""}
     req = r.post("http://0.0.0.0:5000/search_user", data=values)
