632a598edefb6a937ed72fe15d1efb82bf3dc899
fourbet <ofourbet@yahoo.fr>
2021-04-09 11:45:33 +0000

    feat(): post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     req = ur.Request(sys.argv[1], data)
     with ur.urlopen(req) as response:
         html = response.read()
-        print(html)
+        print(html.decode())
