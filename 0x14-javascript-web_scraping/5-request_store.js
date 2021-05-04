791ee45644c6ec98663206d8fe7f8712b4a96e9c
fourbet <ofourbet@yahoo.fr>
2021-05-04 13:07:53 +0000

    feat(): loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
