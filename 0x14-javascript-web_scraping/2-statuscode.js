77482598454cd0fcddbc985d00aa394f4cace266
fourbet <ofourbet@yahoo.fr>
2021-05-04 12:16:51 +0000

    feat(): status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,7 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  console.log('code:', response.statusCode);
+  if (error) {
+    console.log('error:', error);
+  } console.log('code:', response.statusCode);
 });
