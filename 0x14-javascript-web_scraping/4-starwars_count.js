98fc0e2ef5e9e3648ba2feccc0d1d33319d1b7d4
fourbet <ofourbet@yahoo.fr>
2021-05-05 14:17:01 +0000

    feat(): star wars wedge antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const count = JSON.parse(body).results.filter((elem) => {
+      return elem.characters.filter((url) => { return url.includes('18'); }).length;
+    }).length;
+    console.log(count);
+    }
+});
